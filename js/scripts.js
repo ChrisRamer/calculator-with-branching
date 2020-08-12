c03b5ea91a669c11a3eabce611728ff03182baac
Chris Ramer <chrisramer.97@gmail.com>
2020-08-12 15:55:11 -0700

    Finished calculator

--- js/scripts.js
+++ js/scripts.js
@@ -15,53 +15,25 @@ const divide = function (number1, number2) {
 };
 
 $(document).ready(function () {
-
-	// Add
-	$("form#add").submit(function (e) { 
+	
+	$("form#calculator").submit(function (e) {
 		e.preventDefault();
-		
-		const number1 = parseInt($("#add1").val());
-		const number2 = parseInt($("#add2").val());
-		const result = add(number1, number2);
+		const number1 = parseInt($("#input1").val());
+		const number2 = parseInt($("#input2").val());
+		const operator = $("input:radio[name=operator]:checked").val();
+		let result;
 
-		//$("#addOutput").text(result); //Outputs text to emtpy div element to display on page
-		alert("The sum is " + result + "!");
-	});
+		if (operator === "add") {
+			result = add(number1, number2);
+		} else if (operator === "subtract") {
+			result = subtract(number1, number2);
+		} else if (operator === "multiply") {
+			result = multiply(number1, number2);
+		} else if (operator === "divide") {
+			result = divide(number1, number2);
+		}
 
-	// Subtract
-	$("form#subtract").submit(function (e) { 
-		e.preventDefault();
-
-		const number1 = parseInt($("#subtract1").val());
-		const number2 = parseInt($("#subtract2").val());
-		const result = subtract(number1, number2);
-
-		//$("#subtractOutput").text(result); //Outputs text to emtpy div element to display on page
-		alert("The difference is " + result + "!");
-	});
-
-	// Multiply
-	$("form#multiply").submit(function (e) { 
-		e.preventDefault();
-		
-		const number1 = parseInt($("#multiply1").val());
-		const number2 = parseInt($("#multiply2").val());
-		const result = multiply(number1, number2);
-
-		//$("#multiplyOutput").text(result); //Outputs text to emtpy div element to display on page
-		alert("The product is " + result + "!");
-	});
-
-	// Divide
-	$("form#divide").submit(function (e) { 
-		e.preventDefault();
-		
-		const number1 = parseInt($("#divide1").val());
-		const number2 = parseInt($("#divide2").val());
-		const result = divide(number1, number2);
-
-		//$("#divideOutput").text(result); //Outputs text to emtpy div element to display on page
-		alert("The quotient is " + result + "!");
+		$("#output").text(result);
 	});
 
 });
